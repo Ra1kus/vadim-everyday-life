96f442a2b3595ddfdd6acf4332b5096899f1bb6b
Ra1kus <ra1kus@mail.ru>
2020-07-08 16:07:09 +0300

    wasd controls

--- js/script.js
+++ js/script.js
@@ -33,13 +33,13 @@ document.addEventListener('keydown', direction);
 let dir;
 
 function direction(event) {
-    if(event.keyCode === 37 && dir !== 'right')
+    if((event.keyCode === 65 || event.keyCode === 37) && dir !== 'right')
         dir = 'left';
-    else if(event.keyCode === 38 && dir !== 'down')
+    else if((event.keyCode === 87 || event.keyCode === 38) && dir !== 'down')
         dir = 'up';
-    else if(event.keyCode === 39 && dir !== 'left')
+    else if((event.keyCode === 68 || event.keyCode === 39) && dir !== 'left')
         dir = 'right';
-    else if(event.keyCode === 40 && dir !== 'up')
+    else if((event.keyCode === 83 || event.keyCode === 40) && dir !== 'up')
         dir = 'down';
 }
 
